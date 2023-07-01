8b7b6181ad739e387e9d0543b2d797a170440807
angelo pedrosa <angelopedrosa00@gmail.com>
2023-06-30 21:07:54 -0300

    initial project

---
+++ gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'dist/styles/main.css': 'src/styles/main.scss'
+                }
+            }
+        }
+    })
+    grunt.loadNpmTasks('grunt-contrib-sass')
+    grunt.registerTask('default', ['sass'])
+}
