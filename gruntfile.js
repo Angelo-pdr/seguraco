a8f644709273a2ea1e67d4619480e59fa82f2e0b
unknown <angelopedrosa00@gmail.com>
2023-07-05 16:49:38 -0300

    feito a resposividade do carosuel

--- gruntfile.js
+++ gruntfile.js
@@ -16,9 +16,42 @@ module.exports = function(grunt){
                 files: ['src/styles/*.scss'],
                 tasks: ['sass']
             }
+        },
+        replace :{
+            dev:{
+                options:{
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.css'
+                        }
+                    ],
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options:{
+                    removeComments: true,
+                    collapseWhitespace: true,
+                },
+                files:{
+                    'prebuild/index.html' : 'src/index.html'
+                }
+            }
         }
     })
     grunt.loadNpmTasks('grunt-contrib-sass')
+    grunt.loadNpmTasks('grunt-replace')
+    grunt.loadNpmTasks('grunt-contrib-htmlmin')
     grunt.loadNpmTasks('grunt-contrib-watch')
     grunt.registerTask('default', ['watch'])
 }
