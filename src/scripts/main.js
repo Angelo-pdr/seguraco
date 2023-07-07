8546e8c8b84a575a86c93c3513a7ac7dd7ea64ac
unknown <angelopedrosa00@gmail.com>
2023-07-06 23:53:10 -0300

    projeto finalizado

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -25,4 +25,40 @@ class Accordion {
 const accodion = new Accordion('.processo__faqList__faqQuestion')
 accodion.init()
 
-const
+class MobileMenu {
+    constructor(mobileMenu, headerList, headerLiks ){
+        this.mobileMenu = document.querySelector(mobileMenu)
+        this.headerList = document.querySelector(headerList)
+        this.headerLiks = document.querySelectorAll(headerLiks)
+        this.activeItemClass= 'active';
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    animadeLinks(){
+        this.headerLiks.forEach((link, index) => {
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.4s ease forwards
+            ${index /7 + 0.3}s`)
+        })
+    }
+
+    handleClick(){
+        this.headerList.classList.toggle(this.activeItemClass)
+        this.animadeLinks()
+    }
+
+    addClickEvent (){
+        this.mobileMenu.addEventListener("click", this.handleClick)
+    }
+
+    initMobile(){
+        if(this.mobileMenu){
+            this.addClickEvent()
+        }
+        return this
+    }
+}
+
+const mobileMenu = new MobileMenu(".header__menu", ".header__nav", ".header__links li")
+mobileMenu.initMobile()
